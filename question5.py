91655ac321c610ea2ce84632db9966d84e6301a1
Gokila-K2593 <gokilakannan2003@gmail.com>
2025-02-18 15:44:43 +0530

    task3

--- question5.py
+++ question5.py
@@ -1,7 +1,6 @@
 def is_palindrome(s):
     s = ''.join(c for c in s if c.isalnum()).lower()
     return s == s[::-1]
-
 s = input("Enter a string: ")
 if is_palindrome(s):
     print("It is a palindrome")
